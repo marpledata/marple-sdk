7c40a9793b56619e4f5c3190d0d45280a5a61c85
Matthias Baert <matthias@getmarple.io>
2022-07-21 18:28:06 +0200

    add test files

---
+++ marple/tests/test_SDK.py
@@ -0,0 +1,24 @@
+import shutil
+import time
+
+# from src.marpletools import Marple
+from marpledata import Marple
+
+ACCESS_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkRrXzRWOWxwYUtRQmZVZ2ZpbzZIciJ9.eyJodHRwczovL21hcnBsZWRhdGEuY29tL2VtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL21hcnBsZS5ldS5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDQzMTQ1Njg1NTIxMjIwNDMyMzMiLCJhdWQiOlsiaHR0cHM6Ly9hcHAuZ2V0bWFycGxlLmlvL2FwaSIsImh0dHBzOi8vbWFycGxlLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2NTg0MTUzMTksImV4cCI6MTY2MTAwNzMxOSwiYXpwIjoiVm81alNlclZoUUxxeWJRT1dkME03aDg2MUU2RE1WVkciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.V7qDEU7t-WweulmXhsTl9OnCZrUTmztM8NwfyROKkaLaEiXkWthgzcuReR7HNt-SsawiOh75LylpQypNMXIm_pEGku4oUBgs3_cNod7-OulbQ2_LLBA6D-4thpGgECrFcCmmAGGCKGVOCjnmd-vjCt-6eZSLcd1s4jiSVb9lhkLsbYEi3zICJMoBuygniO6BIstejzfKNrwDFg3U28Lde6rSYt3dmA4zQ5TWP9ka-sNqSjsJl9-2X4yERoyYLCRnyQe88hMnfXb_w2nypQklYzAVFZtEYm65LzUSoUqfCfw26jxh_VhYo_H0jphLbw-aao6sWtMa4dUa9aLGznflxA'
+EXAMPLE_FILE_PATH = 'tests\\examples_race.csv'
+PROJECT_NAME = 'api-project'
+
+# create a copy of the file
+new_file_path = f'example_{int(time.time())}.csv'
+shutil.copy(EXAMPLE_FILE_PATH, new_file_path)
+
+
+m = Marple(ACCESS_TOKEN)
+
+m.check_connection()
+print('Connection OK')
+
+source_id = m.upload_data_file(new_file_path, 'API_test')
+print(f'Source id: {source_id} uploaded')
+
+share_link = m.get_link(source_id, PROJECT_NAME)
