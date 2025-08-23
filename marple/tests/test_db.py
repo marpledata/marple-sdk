45786888c8a84d7d98fcf8c2f9dea4ddec403a89
Nero Vanbiervliet <nero.vanbiervliet@gmail.com>
2025-08-23 13:55:15 +0200

    test raw queries running

--- marple/tests/test_db.py
+++ marple/tests/test_db.py
@@ -8,3 +8,7 @@ db = DB(ACCESS_TOKEN)
 db.check_connection()
 pprint.pprint(db.get_streams())
 pprint.pprint(db.get_datasets("charles river"))
+
+query = "select path, stream_id, metadata \nfrom mdb_default_dataset\nlimit 10;"
+response = db.post("/query", json={"query": query})
+print(response.json())
