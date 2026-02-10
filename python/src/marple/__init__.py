b6a26d07428ccab53a0255bc74e0c75cb98f4123
Simon De Baere <simon@marpledata.com>
2026-02-10 09:25:13 +0100

    Also expose db

--- python/src/marple/__init__.py
+++ python/src/marple/__init__.py
@@ -1,6 +1,7 @@
+from . import db
 from .db import DB
 from .files import Marple
 from .insight import Insight
 
-__all__ = ["Marple", "DB", "Insight"]
+__all__ = ["Marple", "DB", "Insight", "db"]
 __version__ = "2.2.2.dev3"
