50656c187cdd7277181ec463a952e0965622ddc7
Simon De Baere <simon@marpledata.com>
2026-02-13 16:44:53 +0100

    Simplify

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -24,12 +24,22 @@ autosummary_generate = True
 autodoc_default_options = {
     "members": True,
     "show-inheritance": True,
-    "member-order": "bysource",
+    "member-order": "groupwise",
     "inherited-members": False,
+    "private-members": False,
     "exclude-members": (
+        # Pydantic v2 model methods
         "model_config,model_fields,model_dump,model_dump_json,model_validate,"
-        "model_validate_json,model_copy,dict,json,parse_obj,parse_raw,validate,"
-        "copy,construct,from_orm,schema,schema_json"
+        "model_validate_json,model_validate_strings,model_copy,model_post_init,"
+        "model_rebuild,model_json_schema,model_fields_set,"
+        # Pydantic v1 methods
+        "dict,json,parse_obj,parse_raw,validate,copy,construct,from_orm,schema,schema_json,"
+        # Pydantic BaseModel dunder methods and attributes
+        "__fields__,__validators__,__config__,__fields_set__,"
+        "__pydantic_decorators__,__pydantic_complete__,__pydantic_custom_init__,"
+        "__pydantic_fields_set__,__pydantic_generic_metadata__,__pydantic_parent_namespace__,"
+        "__pydantic_post_init__,__pydantic_private__,__pydantic_self_init__,"
+        "__private_attributes__,__signature__,__version__,__repr_args__,__repr_name__"
     ),
 }
 
