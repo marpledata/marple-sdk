9dec73c00534dac38e0d4e482a4a77e850a24bc0
Simon De Baere <simon@marpledata.com>
2026-02-16 15:19:09 +0100

    Drop files

--- python/src/marple/__init__.py
+++ python/src/marple/__init__.py
@@ -1,7 +1,6 @@
 from . import db
 from .db import DB
-from .files import Marple
 from .insight import Insight
 
-__all__ = ["Marple", "DB", "Insight", "db"]
+__all__ = ["DB", "Insight", "db"]
 __version__ = "3.0.0.dev1"
